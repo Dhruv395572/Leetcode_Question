193ae528bf3fb7893e63d1481e986c5ba3bded6f
Dhruv Kaushik <dhruv.kaushik_cs.aiml24@gla.ac.in>
2025-10-26 22:29:03 +0530

    Create 344-ReverseString

---
+++ 344-ReverseString/344-ReverseString.java
@@ -0,0 +1,15 @@
+// Last updated: 10/26/2025, 10:29:04 PM
+class Solution {
+    public void reverseString(char[] s) {
+     int i=0;
+     int end=s.length-1;
+     while(i<end){
+        char temp=s[i];
+        s[i]=s[end];
+        s[end]=temp;
+        i++;
+        end--;
+     } 
+     return;  
+    }
+}
