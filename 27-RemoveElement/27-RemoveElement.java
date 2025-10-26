82d4a35ae8077ac71b274690a0eb47372820de44
Dhruv Kaushik <dhruv.kaushik_cs.aiml24@gla.ac.in>
2025-10-26 22:34:44 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.java
@@ -0,0 +1,13 @@
+// Last updated: 10/26/2025, 10:34:45 PM
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
