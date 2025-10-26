c7cd25bf64576427436273ede471b4339fb60455
Dhruv Kaushik <dhruv.kaushik_cs.aiml24@gla.ac.in>
2025-10-26 22:29:56 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 10/26/2025, 10:29:57 PM
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+        int temp=x;
+        while(x>0){
+            int digit=x%10;
+            rev=rev*10+digit;
+            x=x/10;
+        }
+        if(temp==rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
