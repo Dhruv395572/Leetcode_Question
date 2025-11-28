f9a3c2eec4f89f290fdbc6c439e7eaceae2b4d0a
Dhruv Kaushik <dhruv.kaushik_cs.aiml24@gla.ac.in>
2025-11-28 21:03:00 +0530

    File updated at 11/28/2025, 9:03:00 PM

--- 342-PowerofFour/342-PowerofFour.java
+++ 342-PowerofFour/342-PowerofFour.java
@@ -1,15 +1,15 @@
-// Last updated: 11/11/2025, 10:25:31 AM
-class Solution {
-    public boolean isPowerOfFour(int n) {
-        if(n<1){
-            return false;
-        }
-        while (n>1){
-            if(n%4!=0){
-                return false;
-            }
-            n=n/4;
-        }
-        return true;
-    }
-}
+// Last updated: 11/28/2025, 9:03:00 PM
+1class Solution {
+2    public boolean isPowerOfFour(int n) {
+3        if(n<1){
+4            return false;
+5        }
+6        while (n>1){
+7            if(n%4!=0){
+8                return false;
+9            }
+10            n=n/4;
+11        }
+12        return true;
+13    }
+14}
