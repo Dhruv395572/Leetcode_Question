6c39844c3227df8915051fe2e604fc83a528420e
Dhruv Kaushik <dhruv.kaushik_cs.aiml24@gla.ac.in>
2025-11-30 22:35:53 +0530

    File updated at 11/30/2025, 10:35:54 PM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,18 +1,18 @@
-// Last updated: 11/16/2025, 1:53:15 PM
-class Solution {
-    public int majorityElement(int[] nums) {
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            count=0;
-            for(int j=0;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
-            if(count>nums.length/2){
-                return nums[i];
-            }
-        }
-       return -1;
-    }
-}
+// Last updated: 11/30/2025, 10:35:54 PM
+1class Solution {
+2    public int majorityElement(int[] nums) {
+3        int count=0;
+4        for(int i=0;i<nums.length;i++){
+5            count=0;
+6            for(int j=0;j<nums.length;j++){
+7                if(nums[i]==nums[j]){
+8                    count++;
+9                }
+10            }
+11            if(count>nums.length/2){
+12                return nums[i];
+13            }
+14        }
+15       return -1;
+16    }
+17}
