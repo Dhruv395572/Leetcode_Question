670d2d996f7ed8902e67edba31509481503dcb88
Dhruv Kaushik <dhruv.kaushik_cs.aiml24@gla.ac.in>
2025-11-30 22:35:12 +0530

    File updated at 11/30/2025, 10:35:13 PM

--- 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
+++ 26-RemoveDuplicatesfromSortedArray/26-RemoveDuplicatesfromSortedArray.java
@@ -1,13 +1,13 @@
-// Last updated: 10/26/2025, 10:34:33 PM
-class Solution {
-    public int removeDuplicates(int[] nums) {
-        int a=0;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]!=nums[a]){
-                a++;
-                nums[a]=nums[i];
-            }
-        }
-        return a+1;
-    }
-}
+// Last updated: 11/30/2025, 10:35:13 PM
+1class Solution {
+2    public int removeDuplicates(int[] nums) {
+3        int a=0;
+4        for(int i=1;i<nums.length;i++){
+5            if(nums[i]!=nums[a]){
+6                a++;
+7                nums[a]=nums[i];
+8            }
+9        }
+10        return a+1;
+11    }
+12}
