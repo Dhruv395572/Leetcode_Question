5dd6daffd091d9b22171351048dd17d030e4e052
Dhruv Kaushik <dhruv.kaushik_cs.aiml24@gla.ac.in>
2025-11-30 22:45:40 +0530

    File updated at 11/30/2025, 10:45:41 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.java
@@ -1,16 +1,16 @@
-// Last updated: 11/20/2025, 3:01:02 PM
-class Solution {
-    public int maxProfit(int[] prices) {
-        int min=9999999;
-        int max=0;
-        for(int price:prices){
-            if(price<min){
-                min=price;
-            }
-            else if (price - min > max){
-                max= price - min;
-            }
-        }
-        return max;
-    }
-}
+// Last updated: 11/30/2025, 10:45:41 PM
+1class Solution {
+2    public int maxProfit(int[] prices) {
+3        int min=9999999;
+4        int max=0;
+5        for(int price:prices){
+6            if(price<min){
+7                min=price;
+8            }
+9            else if (price - min > max){
+10                max= price - min;
+11            }
+12        }
+13        return max;
+14    }
+15}
