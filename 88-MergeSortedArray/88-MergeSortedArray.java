2f21b12a31a71e5c7d12318d8d28428f4ceb1239
Dhruv Kaushik <dhruv.kaushik_cs.aiml24@gla.ac.in>
2025-11-30 22:34:31 +0530

    File updated at 11/30/2025, 10:34:32 PM

--- 88-MergeSortedArray/88-MergeSortedArray.java
+++ 88-MergeSortedArray/88-MergeSortedArray.java
@@ -1,9 +1,9 @@
-// Last updated: 11/15/2025, 10:40:32 PM
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i=0;i<n;i++){
-            nums1[m+i]=nums2[i];
-        }   
-        Arrays.sort(nums1);
-    }
-}
+// Last updated: 11/30/2025, 10:34:32 PM
+1class Solution {
+2    public void merge(int[] nums1, int m, int[] nums2, int n) {
+3        for(int i=0;i<n;i++){
+4            nums1[m+i]=nums2[i];
+5        }   
+6        Arrays.sort(nums1);
+7    }
+8}
