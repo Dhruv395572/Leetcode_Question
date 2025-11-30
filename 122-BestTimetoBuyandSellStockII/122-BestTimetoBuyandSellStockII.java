df40dac61762949b9d6cc9c94ad90920ebffd41d
Dhruv Kaushik <dhruv.kaushik_cs.aiml24@gla.ac.in>
2025-11-30 22:46:18 +0530

    File updated at 11/30/2025, 10:46:19 PM

--- 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.java
+++ 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.java
@@ -1,12 +1,12 @@
-// Last updated: 11/16/2025, 11:44:22 AM
-class Solution {
-    public int maxProfit(int[] prices) {
-        int sum=0;
-        for(int i=1;i<prices.length;i++){
-            if(prices[i]>prices[i-1]){
-                sum+=prices[i]-prices[i-1];
-            }
-        }
-        return sum;
-    }
-}
+// Last updated: 11/30/2025, 10:46:19 PM
+1class Solution {
+2    public int maxProfit(int[] prices) {
+3        int sum=0;
+4        for(int i=1;i<prices.length;i++){
+5            if(prices[i]>prices[i-1]){
+6                sum+=prices[i]-prices[i-1];
+7            }
+8        }
+9        return sum;
+10    }
+11}
