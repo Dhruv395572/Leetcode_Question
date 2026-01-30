f4dca9cd645797e8bf2961b436f4557b41fca21d
Dhruv Kaushik <dhruvkaushik2810@gmail.com>
2026-01-30 15:00:52 +0530

    File updated at 1/30/2026, 3:00:52 PM

--- 704-BinarySearch/704-BinarySearch.java
+++ 704-BinarySearch/704-BinarySearch.java
@@ -1,18 +1,18 @@
-// Last updated: 1/30/2026, 2:18:03 PM
+// Last updated: 1/30/2026, 3:00:52 PM
 1class Solution {
-2    public int search(int[] nums, int target) {
+2    public int searchInsert(int[] nums, int target) {
 3        int st=0;
 4        int end=nums.length-1;
 5        while(st<=end){
 6            int mid=st+(end-st)/2;
 7            if(nums[mid]==target) return mid;
-8            if(target>nums[mid]){
+8            if(nums[mid]<target){
 9                st=mid+1;
 10            }
 11            else{
 12                end=mid-1;
 13            }
 14        }
-15        return -1;
+15        return st;
 16    }
 17}
