a57aa60cff3b534ea17cec3bb9c777cf1c250e89
Dhruv Kaushik <dhruvkaushik2810@gmail.com>
2026-02-05 15:05:43 +0530

    File updated at 2/5/2026, 3:05:42 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,13 +1,18 @@
-// Last updated: 2/5/2026, 2:48:03 PM
+// Last updated: 2/5/2026, 3:05:42 PM
 1class Solution {
-2    public boolean containsDuplicate(int[] nums) {
-3       HashSet<Integer> set=new HashSet<>();
-4       for(int i:nums){
-5        if(set.contains(i)){
-6            return true;
-7        }
-8        set.add(i);
-9       }
-10       return false;
-11    }
-12}
+2    public boolean isAnagram(String s, String t) {
+3        HashMap<Character,Integer>ang=new HashMap<>();
+4        for(char i:s.toCharArray()){
+5            ang.merge(i,1,Integer::sum);
+6        }
+7        for(char i:t.toCharArray()){
+8            ang.merge(i,-1,Integer::sum);
+9        }
+10        for(int i:ang.values()){
+11            if(i!=0){
+12                return false;
+13            }
+14        }
+15        return true;
+16    }
+17}
