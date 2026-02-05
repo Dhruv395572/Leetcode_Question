0bce13bb71e6e51c83dfa8d6f4c0dd7759431292
Dhruv Kaushik <dhruvkaushik2810@gmail.com>
2026-02-05 15:17:59 +0530

    File updated at 2/5/2026, 3:17:58 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.java
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -1,18 +1,15 @@
-// Last updated: 2/5/2026, 3:06:58 PM
+// Last updated: 2/5/2026, 3:17:58 PM
 1class Solution {
 2    public int majorityElement(int[] nums) {
-3        int count=0;
-4        for(int i=0;i<nums.length;i++){
-5            count=0;
-6            for(int j=0;j<nums.length;j++){
-7                if(nums[i]==nums[j]){
-8                    count++;
-9                }
-10            }
-11            if(count>nums.length/2){
-12                return nums[i];
-13            }
-14        }
-15       return -1;
-16    }
-17}
+3        HashMap<Integer,Integer> map=new HashMap<>();
+4        for(int i:nums){
+5            map.merge(i,1,Integer::sum);
+6        }
+7        for (int key : map.keySet()) {
+8            if (map.get(key) > nums.length / 2) {
+9                return key;
+10            } 
+11        }
+12        return -1;
+13    }
+14}
