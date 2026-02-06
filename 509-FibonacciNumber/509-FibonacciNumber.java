a2951bb4719bdbf556bdc615e1cce43ed46d8651
Dhruv Kaushik <dhruvkaushik2810@gmail.com>
2026-02-06 14:47:28 +0530

    File updated at 2/6/2026, 2:47:28 PM

--- 509-FibonacciNumber/509-FibonacciNumber.java
+++ 509-FibonacciNumber/509-FibonacciNumber.java
@@ -1,15 +1,18 @@
-// Last updated: 11/29/2025, 11:25:59 PM
+// Last updated: 2/6/2026, 2:47:28 PM
 1class Solution {
-2    public int fib(int n) {
+2    public static int sequence(int n){
 3        int a=0,b=1,sum=0;
-4        if(n==1){
-5            return n;
-6        }
-7        for(int i=0;i<n-1;i++){
-8            sum=a+b;
-9            a=b;
-10            b=sum;
-11        }
-12        return sum;
+4            if (n == 0) return a;
+5            if (n == 1) return b;
+6
+7            for(int i=2;i<=n;i++){
+8                sum=a+b;
+9                a=b;
+10                b=sum;
+11            }
+12            return sum;
 13    }
-14}
+14    public int fib(int n) {
+15       return sequence(n);
+16    }
+17}
