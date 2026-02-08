f120bc3725cf5fb19005cc4ac58fe8b345b7695d
Dhruv Kaushik <dhruvkaushik2810@gmail.com>
2026-02-08 15:33:38 +0530

    File updated at 2/8/2026, 3:33:37 PM

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,17 +1,16 @@
-// Last updated: 11/17/2025, 9:07:19 PM
-class Solution {
-    public int climbStairs(int n) {
-        if(n<=3){
-            return n;
-        }
-        int a=1;
-        int b=2;
-        int c=0;
-        for(int i=3;i<=n;i++){
-            c=a+b;
-            a=b;
-            b=c;
-        }
-        return c;
-    }
-}
+// Last updated: 2/8/2026, 3:33:37 PM
+1class Solution {
+2    public static int series(int n){
+3        if(n<3) return n;
+4        int a=1,b=2,sum=0;
+5        for(int i=3;i<=n;i++){
+6            sum=a+b;
+7            a=b;
+8            b=sum;
+9        }
+10        return sum;
+11    }
+12    public int climbStairs(int n) {
+13        return series(n);
+14    }
+15}
