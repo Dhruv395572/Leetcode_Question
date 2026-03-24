3a500358a453a349b5f3f6ec9a9a10a60880440f
Dhruv Kaushik <dhruvkaushik2810@gmail.com>
2026-03-24 22:23:52 +0530

    File updated at 3/24/2026, 10:23:54 PM

--- 141-LinkedListCycle/141-LinkedListCycle.java
+++ 141-LinkedListCycle/141-LinkedListCycle.java
@@ -1,34 +1,26 @@
-// Last updated: 3/22/2026, 10:33:38 AM
+// Last updated: 3/24/2026, 10:23:54 PM
 1/**
 2 * Definition for singly-linked list.
-3 * public class ListNode {
+3 * class ListNode {
 4 *     int val;
 5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-13        ListNode dummy=new ListNode(-1);
-14        ListNode temp=dummy;
-15        while(list1!=null && list2!=null){
-16            if(list1.val<=list2.val){
-17                temp.next=list1;
-18                list1=list1.next;
-19            }
-20            else{
-21                temp.next=list2;
-22                list2=list2.next;
-23            }
-24            temp=temp.next;
-25        }
-26        if(list1==null){
-27            temp.next=list2;
-28        }else{
-29            temp.next=list1;
-30        }
-31        return dummy.next;
-32    }
-33}
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public boolean hasCycle(ListNode head) {
+14       ListNode slow=head;
+15       ListNode fast=head;
+16       while(fast!=null && fast.next!=null){
+17        slow=slow.next;
+18        fast=fast.next.next;
+19       if(slow==fast){
+20        return true;
+21       }
+22       }
+23       return false;
+24    }
+25}
